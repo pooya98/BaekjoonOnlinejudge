d0eda819ded1a726e99be770196b1f29ebb71b00
pooya98 <pooya98@naver.com>
2022-01-30 18:35:12 +0900

    10926, 18108

---
+++ 10926/10926.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char a[10000];
+
+	scanf("%s", a);
+
+	printf("%s", a);
+	printf("??!");
+
+	return 0;
+}
---
+++ 18108/18108.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d", &a);
+
+	printf("%d", a - 543);
+
+	return 0;
+}
