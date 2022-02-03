a22e80274c3d392f8d812513f9bfced285e1bc32
pooya98 <pooya98@naver.com>
2022-02-03 23:21:44 +0900

    14681

---
+++ 14681/14681.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int x, y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+
+	if (x * y > 0) {
+		if (x > 0)
+			printf("1");
+		else
+			printf("3");
+	}
+	else {
+		if (x > 0)
+			printf("4");
+		else
+			printf("2");
+	}
+	return 0;
+}
